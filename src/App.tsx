10ed030448b796182e4a21c9d68c0ccf6f592199
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-22 17:42:42 +1100

    adding video grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import CategoryPills from './components/CategoryPills'
 import { categories } from './data/home'
 import PageHeader from './layouts/PageHeader'
+import VideoGridItem from './components/VideoGridItem'
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -14,6 +15,9 @@ const App = () => {
         <div className='overflow-x-hidden px-8 pb-4'>
           <div className='sticky top-0 bg-white z-10 pb-4'>
             <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelectedCategory={setSelectedCategory}/>
+          </div>
+          <div className='grid gap-4 gird-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
+            <VideoGridItem />
           </div>
         </div>
       </div>
