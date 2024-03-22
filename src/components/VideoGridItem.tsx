ed2cd3766a8bb26e9446b23b1a557abe571bb09b
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-22 22:06:34 +1100

    adding changing time locale

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,6 @@
+import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
+
 type VideoGridItemProps = {
   id: string;
   title: string;
@@ -13,6 +16,8 @@ type VideoGridItemProps = {
   videoUrl: string;
 };
 
+const VIEW_FORMATETTER = new Intl.NumberFormat(undefined, {notation: "compact"})
+
 const VideoGridItem = ({
   id,
   title,
@@ -23,7 +28,33 @@ const VideoGridItem = ({
   thumbnailUrl,
   videoUrl,
 }: VideoGridItemProps) => {
-  return <div>VideoGridItem</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <a href={`/watch?v=${id}`} className="relative aspect-video">
+        <img
+          src={thumbnailUrl}
+          className="block w-full h-full object-cover rounded-xl"
+        />
+        <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+            {formatDuration(duration)}
+        </div>
+      </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img className="w-12 h-12 rounded-full" src={channel.profileUrl}/>
+        </a>
+        <div className="flex flex-col">
+          <a href={`watch?v=${id}`} className="font-bold">{title}</a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {VIEW_FORMATETTER.format(views)} Views . {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoGridItem;
