1c7a6af9de04a623e187d00f42b0be46d2517873
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-22 16:06:09 +1100

    adding category pill

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
 import PageHeader from './layouts/PageHeader'
 
 const App = () => {
   return (
-    <PageHeader />
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+      <div className='grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto'>
+        <div>Sidebar</div>
+        <div className='sticky top-0 bg-white z-10 pb-4'></div>
+      </div>
+    </div>
   )
 }
 
