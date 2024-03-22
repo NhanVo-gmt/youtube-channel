ef1276c62f05b1f74339cad7ddd50cbf5f688c42
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-22 15:48:11 +1100

    adding elements in header bar

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -13,14 +13,14 @@ const PageHeader = () => {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <form className="flex gap-4 flex-grow justify-center">
+      <form className="md:flex hidden gap-4 flex-grow justify-center">
         <div className="flex flex-grow max-w-[600px]">
           <input
             type="search"
             placeholder="Search"
-            className="rounded-l-full border border-secondary-hover shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
           ></input>
-          <Button>
+          <Button className="py-2 px-4 rounded-r-full border border-secondary-border border-l-0 flex-shrink-0">
             <Search />
           </Button>
         </div>
@@ -29,6 +29,12 @@ const PageHeader = () => {
         </Button>
       </form>
       <div className="flex flex-row flex-shrink-0 items-center md:gap-2">
+        <Button size={"icon"} variant={"ghost"} className="md:hidden">
+          <Search />
+        </Button>
+        <Button size={"icon"} variant={"ghost"} className="md:hidden">
+          <Mic />
+        </Button>
         <Button size={"icon"} variant={"ghost"}>
           <Upload />
         </Button>
